709decbe2a1e0fec972cf2249f209e0fdda76fa9
Brian Mamani <62155144+brnma@users.noreply.github.com>
2023-09-18 21:36:48 -0700

    playing around with haptics

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,62 @@
+import * as Haptics from 'expo-haptics';
+import { useEffect } from 'react';
+import { Button } from 'react-native';
+import { useHaptics } from 'react-native-custom-haptics';
+
 import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
+import { PUSLING_HAPTIC } from '../../constants/Haptics';
 
 export default function TabOneScreen() {
+  const { trigger, stop } = useHaptics();
+  useEffect(() => {
+    // stops the haptic pattern on cleanup
+    return () => stop();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <View className="flex items-center justify-center">
       <Text className="text-bold">Tab One</Text>
       <View lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
+      {/* below are example buttons of haptics */}
+      <Text>CUSTOM</Text>
+      <Button title="Custom" onPress={() => trigger(PUSLING_HAPTIC)} />
+      <Text>SELECTION</Text>
+      <Button title="Selection" onPress={() => Haptics.selectionAsync()} />
+      <Text>NOTIFICATIONS</Text>
+      <Button
+        title="Success"
+        onPress={() =>
+          Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success)
+        }
+      />
+      <Button
+        title="Error"
+        onPress={() =>
+          Haptics.notificationAsync(Haptics.NotificationFeedbackType.Error)
+        }
+      />
+      <Button
+        title="Warning"
+        onPress={() =>
+          Haptics.notificationAsync(Haptics.NotificationFeedbackType.Warning)
+        }
+      />
+      <Text>IMPACT</Text>
+      <Button
+        title="Light"
+        onPress={() => Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light)}
+      />
+      <Button
+        title="Medium"
+        onPress={() => Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium)}
+      />
+      <Button
+        title="Heavy"
+        onPress={() => Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy)}
+      />
     </View>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,6 +8,7 @@ import { useFonts } from 'expo-font';
 import { SplashScreen, Stack } from 'expo-router';
 import { useEffect } from 'react';
 import { useColorScheme } from 'react-native';
+import { HapticsProvider } from 'react-native-custom-haptics';
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -51,10 +52,13 @@ function RootLayoutNav() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-      </Stack>
+      {/*HapticsProvider is required to provide custom Haptics */}
+      <HapticsProvider>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+        </Stack>
+      </HapticsProvider>
     </ThemeProvider>
   );
 }
