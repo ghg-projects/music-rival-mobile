b07e5cc2f16496153ba8ade78458190cebbe87e0
Brian Mamani <62155144+brnma@users.noreply.github.com>
2023-09-24 02:19:54 -0700

    add dotenv package and format code

--- babel.config.js
+++ babel.config.js
@@ -19,6 +19,14 @@ module.exports = function (api) {
           },
         },
       ],
+      [
+        'module:react-native-dotenv',
+        {
+          envName: 'APP_ENV',
+          moduleName: '@env',
+          path: '.env',
+        },
+      ],
     ],
   };
 };
